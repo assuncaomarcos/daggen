1660747920b86c58427d5ad2d44447c96ac8c05c
Marcos Assuncao <assuncao@acm.org>
2023-07-21 09:50:56 -0400

    Version 0.0.5

--- daggen/__init__.py
+++ daggen/__init__.py
@@ -1,6 +1,11 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+"""
+This package generates directed acyclic graphs (DAGs) that represent
+a workflow of tasks that are executed on compute resources.
+The generated DAGs can be imported into networkx or igraph.
+"""
 
-__version__ = '0.0.4'
+__version__ = '0.0.5'
 
 from daggen.dag import *
